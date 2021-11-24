d357052e974e5b5eec7d3ec64984ea2a35e0e63a
Kerry Eames <Kerryeames@live.com>
2021-11-23 18:47:33 -0700

    added image and link to first project

---
+++ Assets/js/script.js
@@ -0,0 +1,5 @@
+// var project1 = document.getElementById("project1");
+//
+// project1.addEventListener("click", function () {
+//   location.href("https://kerryeames-vris.github.io/ClassProject1-FeedMe/");
+// });
